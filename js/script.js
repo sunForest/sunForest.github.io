a8e68f579670b879031357794a57f91212115d99
sunForest <sensun@live.cn>
2016-10-16 22:43:39 +0200

    updated projects & skills

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
+$(document).ready(function(){
+    var hash = window.location.hash;
+    if (hash) {
+        var sections = $("section");
+        sections.each(function(index){
+                $(this).removeClass("active");
+            }
+        );
+        $("section" + hash).addClass("active");
+    }
+});
+
 $("nav li").on("click", function(){
 	var index = parseFloat($(this).data("index"));
 	var sections = $("section");
@@ -5,7 +17,7 @@ $("nav li").on("click", function(){
 			$(this).removeClass("active");
 		}
 	);
-	$(sections[index]).show(300, function(){
+	$(sections[index]).show(100, function(){
 		$(sections[index]).addClass("active");
 	});
 });
